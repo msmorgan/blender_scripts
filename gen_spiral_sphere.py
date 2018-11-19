3da71681e4f05a1c98923ed7299b04f6670eadbb
Michael Morgan <michael.morgan@mind-over-data.com>
2018-11-19 17:48:17 -0600

    generate a spiralled UV sphere with BMesh

---
+++ gen_spiral_sphere.py
@@ -0,0 +1,63 @@
+import bmesh
+import bpy
+import math
+import mathutils
+
+ring_count = 16
+segments = ring_count * 2
+size = 1.0
+
+scene = bpy.context.scene
+
+mesh = bpy.data.meshes.new('SpiralSphere')
+spiral_sphere = bpy.data.objects.new('SpiralSphere', mesh)
+
+scene.objects.link(spiral_sphere)
+scene.objects.active = spiral_sphere
+spiral_sphere.select = True
+
+bm = bmesh.new()
+
+prev_ring = None
+ring = []
+prev_prev_last = None
+
+for r in range(ring_count):
+    ring = []
+
+    for s in range(segments):
+        xy_theta = s * 2 * math.pi / segments
+        z_theta = (r + (s / segments)) * math.pi / ring_count
+
+        x = math.cos(xy_theta) * math.sin(z_theta)
+        y = math.sin(xy_theta) * math.sin(z_theta)
+        z = math.cos(z_theta)
+
+        vert = bm.verts.new((x, y, z))
+        ring.append(vert)
+
+    if r == 1:
+        bm.faces.new([prev_ring[segments - 1], ring[0], prev_ring[0]])
+    elif r > 1:
+        bm.faces.new([prev_ring[segments - 1], ring[0], prev_ring[0], prev_prev_last])
+
+    for s in range(segments - 1):
+        if r == 0:
+            if s != 0:
+                bm.faces.new([ring[s], ring[s+1], ring[0]])
+        else:
+            bm.faces.new([ring[s], ring[s+1], prev_ring[s+1], prev_ring[s]])
+
+    if prev_ring is not None:
+        prev_prev_last = prev_ring[segments - 1]
+    prev_ring = ring
+
+bottom = bm.verts.new((0.0, 0.0, -1.0))
+
+bm.faces.new([bottom, prev_ring[0], prev_prev_last])
+
+for s in range(segments - 1):
+    bm.faces.new([bottom, prev_ring[s+1], prev_ring[s]])
+
+bm.to_mesh(mesh)
+bm.free()
